74d0c3d095da126a5532bfa96a830b76f25877b0
chiehchun.lee <chiehchun.lee@jkos.com>
2020-12-29 15:58:35 +0800

    index demo

--- JKOJSBridge/JKOJSBridge/Playground/Pages/MainPage/index.js
+++ JKOJSBridge/JKOJSBridge/Playground/Pages/MainPage/index.js
@@ -1,29 +1,6 @@
-
-function readMoreClicked(id) {
-  if (id == "readme") {
-    var name = JKOAccount.getName();
-      JKBDom.updateComponent(id,name);
-    return 1;
-  }
-  return 0;
-}
-
-function onChangeTitle(title) {
-    Page.pageData.readmeName = "that's hurt~"
-}
-function read2MoreClicked(id) {
-  if (id == "readme2") {
-    var name = JKOAccount.getName();
-      JKBRouter.navigateTo("index2");
-    return 1;
-  }
-  return 0;
-}
-
 Page.onLoad(()=>{
     JKMonitor.log("index1 onload");
     JKMonitor.log("index1 data :"+miniapp._globalData.name);
-    JKBInterface.setNavigationBarTitle("街口功能演示");
 }).onShow(()=>{
     JKMonitor.log("index1 onShow");
     JKMonitor.log("index1 data :"+miniapp._globalData.name);
@@ -33,6 +10,13 @@ Page.onLoad(()=>{
     JKMonitor.log("index1 onHide");
 }).onPageData(()=>{
     return {
-        "readmeName" : "click me!"
+        "myName" : "Daniel"
     }
 });
+
+function onUpdateMyName(title) {
+    Page.pageData.myName = "Chieh Chun Lee"
+}
+function gotoNextPage(title) {
+    JKBRouter.navigateTo("index2");
+}
